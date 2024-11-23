c9133e45c7f34452a59b52f1f0d1a201b6da74c0
nmduonggg <nmduong2104@gmail.com>
2024-11-23 19:07:27 +0900

    update

--- bert/options/config.py
+++ bert/options/config.py
@@ -18,8 +18,8 @@ class ConfigBertLarge:
     NUM_LABELS = 5  # SST-5 classes
     MAX_LENGTH = 128
     BATCH_SIZE = 32
-    LEARNING_RATE = 1e-4
-    EPOCHS = 20
+    LEARNING_RATE = 1e-5
+    EPOCHS = 10
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     LORA = False
     CHECKPOINT = f"best_{MODEL_NAME}{'_lora' if LORA else ''}.pth"
@@ -41,8 +41,8 @@ class ConfigBertLargeLora:
     MODEL_NAME = 'bert-large-uncased'
     NUM_LABELS = 5  # SST-5 classes
     MAX_LENGTH = 128
-    BATCH_SIZE = 128
-    LEARNING_RATE = 1e-4
+    BATCH_SIZE = 32
+    LEARNING_RATE = 1e-5
     EPOCHS = 10
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     LORA = True
